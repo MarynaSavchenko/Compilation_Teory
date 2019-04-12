0d250405767d3b554f2dca4725fe1612d67ad788
MarynaSavchenko <marinacadimovna97@gmail.com>
2019-04-13 00:10:20 +0200

    Add examples to folder

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import scaner  # scanner.py is a file you create, (it is not an external library
 if __name__ == '__main__':
 
     try:
-        filename = sys.argv[1] if len(sys.argv) > 1 else "example_full.txt"
+        filename = sys.argv[1] if len(sys.argv) > 1 else "examples/example_full.txt"
         file = open(filename, "r")
     except IOError:
         print("Cannot open {0} file".format(filename))
